ed6069a934a6d4a76acbba195b5e21adcedbb466
Logon Aluno <logonrmlocal@fiap.com.br>
2019-01-30 20:19:06 -0200

    Initial commit

---
+++ app/src/main/java/com/example/logonrmlocal/recycleview/api/ClientAPI.kt
@@ -0,0 +1,48 @@
+package com.example.logonrmlocal.recycleview.api
+
+import android.util.Log
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+
+class ClientApi<T> {
+
+    fun getClient(c: Class<T>): T {
+        val retrofit = Retrofit.Builder()
+                .client(getOkhttpClientAuth().build())
+                .baseUrl("https://pokedexdx.herokuapp.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+
+        return retrofit.create(c)
+    }
+}
+
+fun getOkhttpClientAuth() : OkHttpClient.Builder{
+    return OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor())
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+}
+
+
+class AuthInterceptor : Interceptor{
+    override fun intercept(chain: Interceptor.Chain?): Response {
+        val requestBuilder = chain!!.request().newBuilder()
+        requestBuilder.addHeader("Authorization", "Basic cG9rZWFwaTpwb2tlbW9u")
+        val request = requestBuilder.build()
+        val response = chain.proceed(request)
+        if (response.code() == 401) {
+            Log.e("MEUAPP", "Error API KEY")
+        }
+        return response
+    }
+
+}
